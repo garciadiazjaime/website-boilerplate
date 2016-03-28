c3710a997310eadeefc84a756468352ffb7427c6
jaime <garciadiazjaime@gmail.com>
2016-03-28 12:35:31 -0500

    include ga

--- src/shared/components/AppHandler.js
+++ src/shared/components/AppHandler.js
@@ -12,6 +12,7 @@ export default class AppHandler extends React.Component {
   componentDidMount() {
     this.scrollHandler(true);
     window.addEventListener('scroll', this.onScroll, false);
+    // this.googleAnalytics();
   }
 
   componentDidUpdate() {
@@ -20,6 +21,17 @@ export default class AppHandler extends React.Component {
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  googleAnalytics() {
+    /*eslint-disable */
+    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    // ga('create', 'UA--', 'auto');
+    // ga('send', 'pageview');
+    /*eslint-enable */
   }
 
   onScroll() {
