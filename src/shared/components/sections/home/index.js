0dc413b641028b8678b3929ef6ac283f0123ad40
jaime <garciadiazjaime@gmail.com>
2016-05-28 13:07:05 -0500

    set block1

--- src/shared/components/sections/home/index.js
+++ src/shared/components/sections/home/index.js
@@ -1,14 +1,12 @@
 /* eslint max-len: [2, 500, 4] */
 import React from 'react';
 
-// import Banner1 from './banner1';
+import Block1 from './block1';
 // import Banner2 from './banner2';
 // import Block2 from './block2';
 // import Block3 from './block3';
 // import Block5 from './block5';
-const styleBlock1 = require('./banner1/style.scss');
 import data from './data';
-import Carousel1 from 'lib-element-catalogue/build/carousel/carousel1';
 
 export default class HomeSection extends React.Component {
   // <Banner1 data={data.banner1} />
@@ -18,7 +16,7 @@ export default class HomeSection extends React.Component {
   // <Block5 data={data.block5} />
   render() {
     return (<div>
-      <Carousel1 style={styleBlock1} data={data.banner1} />
+      <Block1 data={data.block1} />
     </div>);
   }
 }
