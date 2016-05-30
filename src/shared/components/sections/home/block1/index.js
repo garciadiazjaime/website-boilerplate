0ef54d9aa85f249bd282207f19e53a691674fc27
jaime <garciadiazjaime@gmail.com>
2016-05-30 17:09:02 -0500

    adjust style

--- src/shared/components/sections/home/block1/index.js
+++ src/shared/components/sections/home/block1/index.js
@@ -7,14 +7,6 @@ export default class Block1 extends React.Component {
 
   render() {
     const { data } = this.props;
-    // const carouselClasses = {
-    //   inner: style.inner,
-    //   controls: {
-    //     base: style.controls,
-    //     prev: style.prev,
-    //     next: style.next,
-    //   },
-    // };
     return (<div className={style.feature}>
       <div className={style.carouselContainer + ' container-fluid'}>
         <Carousel1 id="carousel-home-block1" classNames={style} data={data} />
